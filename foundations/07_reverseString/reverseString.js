1722ba57a7bdf68320b45682900518bc7a1ac411
Dontwearatie <215163956+StudyBoyIt@users.noreply.github.com>
2026-04-20 14:47:10 +0300

    finished reverseString task

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,5 +1,18 @@
-const reverseString = function() {
+const reverseString = function(word) {
+    let stringBox = [];
+    let reverseString = "";
 
+    // Butting each letter in a list
+    for (let eachLetter = 0; eachLetter < word.length; eachLetter++) {
+        stringBox.push(word[eachLetter])
+    };
+    stringBox = stringBox.reverse();
+    
+    // taking each letter out of the reversed list
+    for (let eachLetter = 0; eachLetter < word.length; eachLetter++) {
+        reverseString += stringBox[eachLetter]
+    };
+    return reverseString;
 };
 
 // Do not edit below this line
