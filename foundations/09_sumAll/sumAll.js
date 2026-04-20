e7b1bc4e8165d879ff88f6b0a17108a17f819de5
Dontwearatie <215163956+StudyBoyIt@users.noreply.github.com>
2026-04-20 17:01:42 +0300

    Last two failed at, need to rereview this later!

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,6 +1,22 @@
-const sumAll = function() {
+const sumAll = function(...arg) {
+    let list = arg.slice().sort()
+    let summed = 0
+    
 
+    for (let repeat = list[0]; repeat <= list.at(-1); repeat++){
+        if (repeat < 0 || repeat % 1 !== 0 || !Number.isInteger(repeat)){
+            return "ERROR";
+        }
+        else {
+        summed += repeat;
+    }};
+    return summed;
 };
+
+console.log(sumAll(10, "90"))
+
+
+
 
 // Do not edit below this line
 module.exports = sumAll;
