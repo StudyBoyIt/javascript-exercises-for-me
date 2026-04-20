84b3df526f74ff894f680b038fc0036df970513c
Dontwearatie <215163956+StudyBoyIt@users.noreply.github.com>
2026-04-20 15:31:41 +0300

    learned about ...args

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,5 +1,19 @@
-const removeFromArray = function() {
+// [1, 2, 3, 4], 3
+
+const removeFromArray = function(list, ...arg) {
+    let newList = []
+    for (let eachNumber = 0; eachNumber < list.length; eachNumber++){
+        if (arg.includes(list[eachNumber])){
+            continue
+        }
+    else {
+        newList.push(list[eachNumber])
+    }}
+    return newList
+
 };
+
+console.log(removeFromArray([1,2, 2, 3, 4], 2, 3))
 
 // Do not edit below this line
 module.exports = removeFromArray;
