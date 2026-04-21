6f954aed0a7059e17b8c39ea1ed61ef3068b6fbe
Dontwearatie <215163956+StudyBoyIt@users.noreply.github.com>
2026-04-21 13:01:59 +0300

    Finished task

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahren) {
+  let calc = (fahren - 32) / 1.8
+  let rounded = Number((calc).toFixed(1))
+  return rounded
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  let calc = (celsius * 1.8) + 32
+  let rounded = Number((calc).toFixed(1))
+  return rounded
 };
 
 // Do not edit below this line
