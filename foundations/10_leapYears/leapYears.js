bf77abe0798e9b497d32adfb7d6ce039e2c1f270
Dontwearatie <215163956+StudyBoyIt@users.noreply.github.com>
2026-04-21 12:26:55 +0300

    Task finished

--- foundations/10_leapYears/leapYears.js
+++ foundations/10_leapYears/leapYears.js
@@ -1,5 +1,16 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    if (Number.isInteger(year / 4) && !Number.isInteger(year / 100)){
+        return true
+        // return `${year} is leap year!`
+    }
+    else if (Number.isInteger(year /100) && Number.isInteger(year / 400)){
+        return true
+        // return `${year} is leap year!`
+    }
+    else {
+        return false
+        // return `${year} is not leap year!`
+    }
 };
 
 // Do not edit below this line
